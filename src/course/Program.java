e41528ee06ba80bab65aafb83f7ac120fd924f4d
Robson Marinho <robson_luz15@hotmail.com>
2019-03-25 23:59:32 -0300

    Subindo primeira aula do treinamento de Java

---
+++ src/course/Program.java
@@ -0,0 +1,16 @@
+//Declaração de pacote
+package course;
+
+import java.util.Scanner;
+
+//Criando a classe
+public class Program {
+	// Criando o main
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Hello world!");
+	}
+
+}
