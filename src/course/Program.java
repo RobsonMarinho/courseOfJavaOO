c610af45676ec0c3e978d428f4aad7be55499f4b
Robson Marinho <robson_luz15@hotmail.com>
2019-03-28 01:38:42 -0300

    Pimitive types

--- src/course/Program.java
+++ src/course/Program.java
@@ -7,10 +7,27 @@ import java.util.Scanner;
 public class Program {
 	// Criando o main
 	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Hello world!");
+
+		boolean completed = false;
+		char gender = 'F';
+		char letter = '\u0041';
+		byte n1 = 127;// Ocupa 8 bits da memória
+		int n2 = 126;// Ocupa 32 bits da memória
+		int n3 = 2147483647;// Ocupa 32 bits da memória
+		long n4 = 2147483648L;// Ocupa 64 bits da memória
+		float n5 = 4.5f; // Ocupa 32 bits da memória
+		double n6 = 4.5;
+
+		// Exibir no console as varuáves declaradas
+		System.out.println(completed);
+		System.out.println(gender);
+		System.out.println(letter);
+		System.out.println(n1);
+		System.out.println(n2);
+		System.out.println(n3);
+		System.out.println(n4);
+		System.out.println(n5);
+		System.out.println(n6);
 	}
 
 }
---
+++ src/course/ProgramaStringObjects.java
@@ -0,0 +1,15 @@
+package course;
+
+public class ProgramaStringObjects {
+
+	public static void main(String[] args) {
+		String name = "Maria Green";
+		Object obj1 = "Alex Brown";
+		Object obj2 = 4.5f;
+
+		System.out.println(name);
+		System.out.println(obj1);
+		System.out.println(obj2);
+	}
+
+}
