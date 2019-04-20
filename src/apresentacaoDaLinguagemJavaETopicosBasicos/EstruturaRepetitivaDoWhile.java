423613218b4ad5db39903e983c78b9a6d2c5dba8
Robson Marinho <robson_luz15@hotmail.com>
2019-04-20 17:48:41 -0300

    Estrutura Repetitiva Do-While

---
+++ src/apresentacaoDaLinguagemJavaETopicosBasicos/EstruturaRepetitivaDoWhile.java
@@ -0,0 +1,24 @@
+package apresentacaoDaLinguagemJavaETopicosBasicos;
+
+import java.util.Scanner;
+
+public class EstruturaRepetitivaDoWhile {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		char resp;
+		do {
+			System.out.println("Enter a number: ");
+			double n = sc.nextDouble();
+			double sq = Math.sqrt(n);
+			System.out.printf("Square root = %.3f%n", sq);
+
+			System.out.println("Repeat (y/n)? ");
+			resp = sc.next().charAt(0);
+		} while (resp != 'n');
+
+		sc.close();
+	}
+
+}
