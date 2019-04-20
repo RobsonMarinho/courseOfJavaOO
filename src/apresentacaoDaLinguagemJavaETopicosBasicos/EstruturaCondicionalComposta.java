8d024b8abd1ece0658ff02cf5421180ddba8dd86
Robson Marinho <robson_luz15@hotmail.com>
2019-04-19 21:36:22 -0300

    Estrutura Condicional Composta

---
+++ src/apresentacaoDaLinguagemJavaETopicosBasicos/EstruturaCondicionalComposta.java
@@ -0,0 +1,23 @@
+package apresentacaoDaLinguagemJavaETopicosBasicos;
+
+import java.util.Scanner;
+
+public class EstruturaCondicionalComposta {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter an integer number: ");
+		int n = sc.nextInt();
+
+		if (n % 2 == 0) {
+			System.out.println("Even!");
+		} else {
+			System.out.println("Odd!");
+		}
+
+		sc.close();
+	}
+
+}
