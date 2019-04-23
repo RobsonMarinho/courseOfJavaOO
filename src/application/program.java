af0f3bb4c0deb0d68f970a5b1cebf5c5d16680a1
Robson Marinho <robson_luz15@hotmail.com>
2019-04-23 00:58:35 -0300

    Object e toString

--- src/application/program.java
+++ src/application/program.java
@@ -21,7 +21,7 @@ public class program {
 		System.out.println("Quantity in stock: ");
 		product.quantity = sc.nextInt();
 
-		System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+		System.out.println(product);
 
 		sc.close();
 	}
