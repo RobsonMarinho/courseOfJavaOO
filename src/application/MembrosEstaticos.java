4f1dcc5ddbb520482df4cb67ee248a99d69ee0f1
Robson Marinho <robson_luz15@hotmail.com>
2019-04-24 23:46:57 -0300

    Inserindo valor PI na classe membros estáticos

--- src/application/MembrosEstaticos.java
+++ src/application/MembrosEstaticos.java
@@ -3,33 +3,28 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import util.Calculator;
+
 public class MembrosEstaticos {
 
 	public static void main(String[] args) {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+		Calculator calc = new Calculator();
+
 		System.out.println("Enter radius");
 		double radius = sc.nextDouble();
-		
-		double c = circumference(radius);
-		
-		double v = volume(radius);
-		
-		System.out.println("Circumference: %.2f%n", c);
-		System.out.println("Volume: %.2f%n", v);
-		System.out.println("PI value: %.2f%n", PI);
-		
-		
+
+		double c = calc.circumference(radius); // Calcula a circunferencia
+
+		double v = calc.volume(radius); // Calcula o volume
+
+		System.out.printf("Circumference: %.2f%n", c);
+		System.out.printf("Volume: %.2f%n", v);
+		System.out.printf("PI value: %.2f%n", calc.PI);
+
 		sc.close();
 	}
 
-	public static double circumference(double radius) {
-		return 2.0 * PI * radius;
-	}
-	
-	public static double volume(double radius) {
-		return 4.0 * PI * radius * radius * radius / 3.0;
-	}
 }
