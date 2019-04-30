2c755f6d2069872e0b538dc2ffcff73d7aaea07f
Robson Marinho <robson_luz15@hotmail.com>
2019-04-30 00:37:34 -0300

    Criando construtores

--- src_ConstrutoresPalavraThisSobrecargaEncapsulamento/applications/Program.java
+++ src_ConstrutoresPalavraThisSobrecargaEncapsulamento/applications/Program.java
@@ -3,44 +3,43 @@ package applications;
 import java.util.Locale;
 import java.util.Scanner;
 import entities.Product;
+import entities.Products;
 
 public class Program {
 	public static void main(String[] args) {
 
-				Locale.setDefault(Locale.US);
-				Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
-				Product product = new Product();
-				System.out.println("Enter product data: ");
-				System.out.println("name: ");
-				product.name = sc.nextLine();
-				System.out.print("price: ");
-				product.price = sc.nextDouble();
-				System.out.println("Quantity in stock: ");
-				product.quantity = sc.nextInt();
+		System.out.println("Enter product data: ");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Price: ");
+		double price = sc.nextDouble();
+		System.out.print("Quantity in stock: ");
+		int quantity = sc.nextInt();
+		Products products = new Products(name, price, quantity);
 
-				System.out.println();
-				System.out.println("Product data: " + product);
+		System.out.println();
+		System.out.println("Product data: " + products);
 
-				System.out.println();
-				System.out.println("Enter the number of products to be added in stock: ");
-				int quantity = sc.nextInt();
-				product.addProducts(quantity);
+		System.out.println();
+		System.out.println("Enter the number of products to be added in stock: ");
+		quantity = sc.nextInt();
+		products.addProducts(quantity);
 
-				System.out.println();
-				System.out.println("Update data: " + product);
+		System.out.println();
+		System.out.println("Update data: " + products);
 
-				System.out.println();
-				System.out.println("Enter the number of products to be removed from stock: ");
-				quantity = sc.nextInt();
-				product.removeProducts(quantity);
+		System.out.println();
+		System.out.println("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		products.removeProducts(quantity);
 
-				System.out.println();
-				System.out.println("Update data: " + product);
+		System.out.println();
+		System.out.println("Update data: " + products);
 
-				sc.close();
-			}
+		sc.close();
+	}
 
-		}
-
-}}
+}
--- src_ConstrutoresPalavraThisSobrecargaEncapsulamento/entities/Products.java
+++ src_ConstrutoresPalavraThisSobrecargaEncapsulamento/entities/Products.java
@@ -1,5 +1,32 @@
 package entities;
 
 public class Products {
+	// Atributos
+	public String name;
+	public double price;
+	public int quantity;
 
+	public Products(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	public double totalValueInStock() {
+		return price * quantity;
+	}
+
+	public void addProducts(int quantity) {
+
+		this.quantity += quantity; // This referencia ao objeto
+	}
+
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity;
+	}
+
+	public String toString() {
+		return name + ", $ " + String.format("%.2f", price) + ", " + quantity + " units, Total: $ "
+				+ String.format("%.2f", totalValueInStock());
+	}
 }
