97ffb432ab22827def5280591635d3e87abc2560
Robson Marinho <robson_luz15@hotmail.com>
2019-07-12 23:32:29 -0300

    Gerando automaticamente construtores, getters e Setters com Eclipse

--- src_ConstrutoresPalavraThisSobrecargaEncapsulamento/entities/Products.java
+++ src_ConstrutoresPalavraThisSobrecargaEncapsulamento/entities/Products.java
@@ -6,18 +6,16 @@ public class Products {
 	private double price;
 	private int quantity;
 
-	// Construtor
 	public Products() {
+
 	}
 
-	// Construtor
 	public Products(String name, double price, int quantity) {
 		this.name = name;
 		this.price = price;
 		this.quantity = quantity;
 	}
 
-	// Construtor (Sobrecarga)
 	public Products(String name, double price) {
 		this.name = name;
 		this.price = price;
@@ -27,7 +25,6 @@ public class Products {
 		return name;
 	}
 
-	// Método para setar nome
 	public void setName(String name) {
 		this.name = name;
 	}
