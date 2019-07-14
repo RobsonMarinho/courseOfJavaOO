aafc7043c0479f46276d5dd615d1f5d188f66c69
Robson Marinho <robson_luz15@hotmail.com>
2019-07-14 00:09:26 -0300

    Tipos referência VS tipos valor

---
+++ src/comportamentoDeMemoriaArrayListas/Program.java
@@ -0,0 +1,14 @@
+package comportamentoDeMemoriaArrayListas;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// Declarando variável
+		int p;
+		// Inicializando variável
+		p = 2;
+
+		System.out.println(p);
+	}
+
+}
