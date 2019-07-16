f4170103e5610529b97c5c62bb3830445e56621d
Robson Marinho <robson_luz15@hotmail.com>
2019-07-15 23:59:01 -0300

    Vector 2

--- src/comportamentoDeMemoriaArrayListas/Program.java
+++ src/comportamentoDeMemoriaArrayListas/Program.java
@@ -3,6 +3,8 @@ package comportamentoDeMemoriaArrayListas;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Products;
+
 public class Program {
 
 	public static void main(String[] args) {
@@ -11,20 +13,22 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 
 		int n = sc.nextInt();
-		double[] vect = new double[n];
-
-		for (int i = 0; i < n; i++) {
-			vect[i] = sc.nextDouble();
+		Products[] vect = new Products[n];
+		
+		for (int i=0; i<n; i++) {
+			sc.nextLine();
+			String name = sc.nextLine();
+			double price = sc.nextDouble();
+			vect[i] = new Products(name, price);
 		}
-
+		
 		double sum = 0.0;
-		for (int i = 0; i < n; i++) {
-			sum += vect[i];
+		for (int i=0; i<n; i++) {
+			sum += vect[i].getPrice();
 		}
 		double avg = sum / n;
 
-		System.out.println("AVERAGE HEIGHT: %.2f%n" + avg);
-
+		System.out.printf("AVERAGE PRICE = %.2f%n", avg);
 		sc.close();
 	}
 
